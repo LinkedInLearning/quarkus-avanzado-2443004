0a496630b990f00223155ccbb117c2ba2f970ab2
Katia Aresti <karesti@redhat.com>
2022-05-25 17:27:40 +0200

    init

---
+++ open-telemetry/consumeSalesService.sh
@@ -0,0 +1,14 @@
+http  localhost:8280'/sales/KE180/availability?units=1'
+http  localhost:8280'/sales/KE250/availability?units=1'
+http  localhost:8280'/sales/KI20K/availability?units=1'
+http post localhost:8280/sales customerId=c1 sku=KE325X units=12
+http post localhost:8280/sales customerId=c1 sku=KEAA06 units=12
+http post localhost:8280/sales customerId=c1 sku=KE12Li units=12
+http post localhost:8280/sales customerId=c1 sku=KEBL200 units=12
+http post localhost:8280/sales customerId=c1 sku=KEBL600 units=12
+http post localhost:8280/sales customerId=c1 sku=KE9W units=12
+http post localhost:8280/sales customerId=c1 sku=KE20W units=12
+http post localhost:8280/sales customerId=c1 sku=KE23W08 units=12
+http post localhost:8280/sales customerId=c1 sku=KI20K units=12
+http post localhost:8280/sales customerId=c1 sku=KE250 units=12
+http post localhost:8280/sales customerId=c1 sku=KE250 units=12
--- sales-service/src/main/java/com/kineteco/service/SalesService.java
+++ sales-service/src/main/java/com/kineteco/service/SalesService.java
@@ -18,7 +18,8 @@ import java.util.ArrayList;
 public class SalesService {
 
    @Timed(value = "create-customer-command")
-   public CustomerSale createCustomerSale(CustomerCommand command, Product product) {
+   @WithSpan
+   public CustomerSale createCustomerSale(@SpanAttribute(value = "customerCommand") CustomerCommand command, Product product) {
       Customer customer = Customer.findByCustomerId(command.getCustomerId())
             .orElseThrow(() -> new CustomerNotFoundException());
       CustomerSale customerSale = new CustomerSale();
