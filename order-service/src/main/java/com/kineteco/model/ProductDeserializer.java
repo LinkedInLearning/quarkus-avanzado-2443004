2aa7279d5e0e0e195f93ee6f98cca23010aa52e0
Katia Aresti <karesti@redhat.com>
2022-06-01 21:29:09 +0200

    init

--- order-service/src/main/java/com/kineteco/model/ProductDeserializer.java
+++
@@ -1,9 +0,0 @@
-package com.kineteco.model;
-
-import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
-
-public class ProductDeserializer extends ObjectMapperDeserializer<Product> {
-    public ProductDeserializer() {
-        super(Product.class);
-    }
-}
