22a7e2e48a8316c970b82e3227e605d95ab0dd96
Katia Aresti <karesti@redhat.com>
2022-06-02 10:52:32 +0200

    init

--- sales-service/src/main/java/com/kineteco/service/ProductSalesGenerator.java
+++ sales-service/src/main/java/com/kineteco/service/ProductSalesGenerator.java
@@ -1,19 +1,13 @@
 package com.kineteco.service;
 
-import com.kineteco.CustomerResource;
 import com.kineteco.model.ProductSale;
 import io.quarkus.scheduler.Scheduled;
-import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 
 @ApplicationScoped
 public class ProductSalesGenerator {
-
-   private static final Logger LOGGER = Logger.getLogger(CustomerResource.class);
-
-
 
    @Scheduled(cron="{kineteco.sales}")
    public void generate() {
