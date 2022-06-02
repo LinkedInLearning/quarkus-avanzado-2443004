ba5b3f265cab8ca18be84a43f340184c6ce85437
Katia Aresti <karesti@redhat.com>
2022-06-02 11:29:08 +0200

    final

--- sales-service/src/main/java/com/kineteco/service/ProductSalesGenerator.java
+++ sales-service/src/main/java/com/kineteco/service/ProductSalesGenerator.java
@@ -2,20 +2,31 @@ package com.kineteco.service;
 
 import com.kineteco.model.ProductSale;
 import io.quarkus.logging.Log;
+import io.quarkus.qute.Location;
+import io.quarkus.qute.Template;
 import io.quarkus.scheduler.Scheduled;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @ApplicationScoped
 public class ProductSalesGenerator {
 
+   @Location("sales/sales-mail-report")
+   Template salesReport;
+
    @Scheduled(cron="{kineteco.sales}")
    public void generate() {
       List<ProductSale> productSales = ProductSale.listAll();
-
-      // Generate Report
-      Log.info(productSales);
+      salesReport.data("sales", productSales)
+            .data("date", LocalDateTime.now().toString())
+            .renderAsync()
+            .thenAccept(r -> Log.info(r))
+            .exceptionally(e -> {
+               Log.error(e);
+               return null;
+            });
    }
 
 }
